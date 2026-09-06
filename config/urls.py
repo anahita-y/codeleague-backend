db9911e1fadf36962b8bb5776d6a89788dcb1aad
Anna <anahita.yazdani84@gmail.com>
2026-09-06 16:38:29 +0330

    Update Urls for config

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/' , include('problems.urls')),
+    path('api/' , include('submissions.urls')),
+    path('api/' , include('users.urls')),
+    path('api-auth/' , include('rest_framework.urls')),
 ]
