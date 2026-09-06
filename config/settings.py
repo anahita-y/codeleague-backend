564e2e5fc0d6560fe7aadc849f12bacfae62af68
Anna <anahita.yazdani84@gmail.com>
2026-09-06 23:27:53 +0330

    Fix INSTALLED_APP comma , and LOGIN_REDIRECT_URL

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework'
+    'rest_framework',
     "users",
     "problems",
     "submissions",
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+LOGIN_REDIRECT_URL = '/api/problems/'
